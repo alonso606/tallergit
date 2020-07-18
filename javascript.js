81c17b90005aa5fc4d7a0151dd86061acde7240d
Alonso Villarreal <alonso.villareal@vlim.com.mx>
2020-07-17 20:51:10 -0500

    nuevo parrafo

---
+++ javascript.js
@@ -0,0 +1,41 @@
+ //  var arreglo =['HTML',14,true];
+//  console.log(arreglo[1]);
+//  var objeto ={
+//      nombre : 'html',
+//      duracion:15 ,
+//      estado :true,
+//      capitulos:{
+//          nombre:'introduccion',
+//          videos:20
+javascript.js
+//      }
+
+//  }
+//  console.log(objeto.capitulos.nombre);
+// var arrayObjetos = [
+//     {
+//         nombre :'html',
+//         estado:true 
+//     },
+
+//     {
+//         nombre :'css',
+//         estado: true 
+//     },
+
+//     {
+//         nombre :'js',
+//         estado: false 
+//     }
+   
+// ]
+// console.log(arrayObjetos[0].nombre);
+// for( let indice  of arrayObjetos){
+//     console.log(indice.nombre);
+
+// }
+var dolar = [{"fecha":"2020-06-19T04:00:00.000Z","valor":812.19},{"fecha":"2020-06-18T04:00:00.000Z","valor":798.53},{"fecha":"2020-06-17T04:00:00.000Z","valor":779.1},{"fecha":"2020-06-16T04:00:00.000Z","valor":796.87},{"fecha":"2020-06-15T04:00:00.000Z","valor":790.73},{"fecha":"2020-06-12T04:00:00.000Z","valor":784.65},{"fecha":"2020-06-11T04:00:00.000Z","valor":767.93},{"fecha":"2020-06-10T04:00:00.000Z","valor":771.96},{"fecha":"2020-06-09T04:00:00.000Z","valor":769.26},{"fecha":"2020-06-08T04:00:00.000Z","valor":764.97},{"fecha":"2020-06-05T04:00:00.000Z","valor":775.58},{"fecha":"2020-06-04T04:00:00.000Z","valor":769.13},{"fecha":"2020-06-03T04:00:00.000Z","valor":782.86},{"fecha":"2020-06-02T04:00:00.000Z","valor":796.46},{"fecha":"2020-06-01T04:00:00.000Z","valor":806.32},{"fecha":"2020-05-29T04:00:00.000Z","valor":812.74},{"fecha":"2020-05-28T04:00:00.000Z","valor":816.47},{"fecha":"2020-05-27T04:00:00.000Z","valor":802.1},{"fecha":"2020-05-26T04:00:00.000Z","valor":803.74},{"fecha":"2020-05-25T04:00:00.000Z","valor":805.75},{"fecha":"2020-05-22T04:00:00.000Z","valor":806.17},{"fecha":"2020-05-20T04:00:00.000Z","valor":819.08},{"fecha":"2020-05-19T04:00:00.000Z","valor":820.65},{"fecha":"2020-05-18T04:00:00.000Z","valor":823.86},{"fecha":"2020-05-15T04:00:00.000Z","valor":822.93},{"fecha":"2020-05-14T04:00:00.000Z","valor":820.38},{"fecha":"2020-05-13T04:00:00.000Z","valor":821.88},{"fecha":"2020-05-12T04:00:00.000Z","valor":826.05},{"fecha":"2020-05-11T04:00:00.000Z","valor":827.65},{"fecha":"2020-05-08T04:00:00.000Z","valor":836.27},{"fecha":"2020-05-07T04:00:00.000Z","valor":839.08}]
+for (let i of dolar ){
+    console.log(i.valor);
+    
+}
